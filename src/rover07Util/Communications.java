306ab33e1417f366692bd314fb57049710cb2300
meishu <theme97@gmail.com>
2016-05-07 19:09:04 -0700

    Initial commit of Communications utility

---
+++ src/rover07Util/Communications.java
@@ -0,0 +1,194 @@
+package rover07Util;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.*;
+
+public class Communications implements Runnable {
+    private Selector selector;
+    private ByteBuffer buffer;
+    private Set<String> received;
+    private Set<SelectionKey> keysToWrite;
+
+    private ServerSocketChannel serverSocket;
+
+    private final boolean DEBUG = true;
+    private final String DEBUG_PREFIX = "[Communications] ";
+    private void log(String str) {
+        if (DEBUG) {
+            System.out.println(DEBUG_PREFIX + str);
+        }
+    }
+
+    private class SocketIO {
+        public StringBuilder in;
+        public List<ByteBuffer> out;
+
+        SocketIO() {
+            in = new StringBuilder();
+            out = Collections.synchronizedList(new ArrayList<>());
+        }
+    }
+
+    public Communications() throws IOException {
+        selector = Selector.open();
+        buffer = ByteBuffer.allocateDirect(64);
+        received = Collections.synchronizedSet(new HashSet<>());
+        keysToWrite = new HashSet<>();
+
+        serverSocket = ServerSocketChannel.open();
+        serverSocket.bind(new InetSocketAddress("127.0.0.1", 53707));
+        serverSocket.configureBlocking(false);
+        serverSocket.register(selector, SelectionKey.OP_ACCEPT);
+
+        log("listening on " + serverSocket.getLocalAddress());
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (keysToWrite) {
+                for (SelectionKey key : keysToWrite) {
+                    key.interestOps(SelectionKey.OP_WRITE);
+                }
+                keysToWrite.clear();
+            }
+
+            try {
+                selector.select(); // blocks until wakeup()
+            } catch (IOException e) {
+                break; // TODO
+            }
+
+            Iterator<SelectionKey> iter = selector.selectedKeys().iterator();
+            while (iter.hasNext()) {
+                SelectionKey key = iter.next();
+                iter.remove(); // remove event to avoid reprocessing
+
+                if (!key.isValid()) continue;
+
+                try {
+                    if (key.isAcceptable()) {
+                        accept(key);
+                    } else if (key.isReadable()) {
+                        read(key);
+                    } else if (key.isWritable()) {
+                        write(key);
+                    }
+                } catch (IOException e) {
+                    log("closing socket " + key.channel());
+                    log("- reason: " + e.getMessage());
+                    try {
+                        key.channel().close(); // TODO is this needed?
+                    } catch (IOException e2) {
+                    }
+                    key.cancel();
+                }
+            }
+        }
+    }
+
+    private void accept(SelectionKey key) throws IOException {
+        SocketChannel client = serverSocket.accept();
+        client.configureBlocking(false);
+        client.register(selector, SelectionKey.OP_READ, new SocketIO());
+        log("accepted connection from " + client);
+    }
+
+    private void read(SelectionKey key) throws IOException {
+        SocketIO io = (SocketIO)key.attachment();
+        SocketChannel client = (SocketChannel)key.channel();
+
+        // start reading
+        while (true) {
+            // read into buffer
+            buffer.clear();
+            int read = client.read(buffer);
+            if (read == 0) break;
+
+            // flip buffer for writing
+            buffer.flip();
+
+            // convert to byte array
+            byte[] data = new byte[read];
+            buffer.get(data);
+
+            // add to this key's string builder
+            io.in.append(new String(data));
+
+            // start parsing
+            while (true) {
+                // look for eol
+                int eol = io.in.indexOf("\n");
+                if (eol == -1) break;
+
+                // get first line
+                String line = io.in.substring(0, eol);
+
+                // add to parsed lines for later retrieval
+                received.add(line);
+                log("received data: " + line);
+
+                // remove from builder
+                io.in.delete(0, eol + 1);
+            }
+        }
+    }
+
+    private void write(SelectionKey key) throws IOException {
+        SocketIO io = (SocketIO)key.attachment();
+        SocketChannel client = (SocketChannel)key.channel();
+
+        List queue = io.out;
+        synchronized (queue) {
+            while (!queue.isEmpty()) {
+                ByteBuffer buf = (ByteBuffer) queue.get(0);
+                client.write(buf);
+                if (buf.remaining() > 0) break;
+                queue.remove(0);
+            }
+
+            if (queue.isEmpty()) {
+                key.interestOps(SelectionKey.OP_READ);
+            }
+        }
+    }
+
+    public void send(String data) {
+        byte[] byteData = (data + "\n").getBytes();
+        ByteBuffer buf = ByteBuffer.wrap(byteData);
+
+        log("queueing data to write: " + data);
+
+        synchronized (keysToWrite) {
+            for (SelectionKey key : selector.keys()) {
+                if (key.attachment() != null) {
+                    SocketIO io = (SocketIO)key.attachment();
+                    io.out.add(buf);
+                    keysToWrite.add(key);
+                }
+            }
+        }
+
+        selector.wakeup();
+    }
+
+    public Set<String> getReceiveQueue() {
+        Set<String> queue;
+        synchronized(received) {
+            queue = new HashSet<>(received);
+            received.clear();
+        }
+        return queue;
+    }
+
+    public static void main(String[] args) {
+        try {
+            Communications test = new Communications();
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
